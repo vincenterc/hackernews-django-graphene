7e83babd3359261a5a0daa7433771ec69512bfa6
vincenterr <chung.menghsun@gmail.com>
2019-06-20 09:55:38 +0800

    Modify link and vote models

--- links/models.py
+++ links/models.py
@@ -4,15 +4,21 @@ from django.db import models
 
 # Create your models here.
 class Link(models.Model):
-    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True)
+    url = models.URLField()
     posted_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL,
+        related_name="links",
+        null=True,
+        on_delete=models.CASCADE,
     )
 
 
 class Vote(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     link = models.ForeignKey(
         "links.Link", related_name="votes", on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="votes", on_delete=models.CASCADE
     )
