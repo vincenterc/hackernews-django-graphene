46a54633dd8db544c19419641212c09b87f73a59
vincenterr <chung.menghsun@gmail.com>
2019-06-20 09:56:13 +0800

    Register link and vote models to the admin site

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Link, Vote
+
+
 # Register your models here.
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ("id", "url", "created_at")
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "username")
+
+    def username(self, obj):
+        return obj.user.username
